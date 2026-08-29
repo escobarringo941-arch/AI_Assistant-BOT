5699d471e936ece185fc32f56c8f2fed72408b7f
escobarringo941-arch <escobarringo941@gmail.com>
2026-08-29 11:34:27 +0100

    Update ai_bot.py

--- ai_bot.py
+++ ai_bot.py
@@ -35,7 +35,6 @@ CORE_COGS = [
     "cogs.temp_voice",
     "cogs.temp_music",
     "cogs.voice_runtime",
-    "cogs.camera_room_split",
     "cogs.camera_video_hub",
     "cogs.server_setup",
     "cogs.member_events",
